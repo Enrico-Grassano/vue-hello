41a4bdcdcfdc85369976ac6c2c4d089e08a5107a
Enrico-Grassano <grassano.enrico1996@gmail.com>
2021-05-18 15:04:20 +0200

    Vue-hello (Bonus) completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 //Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+//Bonus: aggiungere  alla pagina un'immagine, presa anch'essa da un dato.
 var app = new Vue ({
         el: "#root",
         data: {
-            message: "Ciao Vue!!!"
+            message: "Ciao Vue!!!",
+            imgUrl: "https://animesher.com/orig/1/108/1087/10876/animesher.com_onizuka-great-teacher-onizuka-gto-1087654.jpg",
+            imgAlt: "G.T.O."
         }
     }
 )
